71b05805ceb62a53f4fde2d6d83d860c762c2dfa
Sagar Balyan <sagarbalyan18@gmail.com>
2019-01-07 21:47:20 +0530

    First Commit : The app is able to generate a QR Code and track the location by reading that QR Code.

---
+++ app/src/main/java/com/wildlabs/tracklocation/CameraActivity.java
@@ -0,0 +1,114 @@
+package com.wildlabs.tracklocation;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.qrcode.encoder.QRCode;
+
+import java.io.IOException;
+
+public class CameraActivity extends AppCompatActivity {
+
+    SurfaceView mSurfaceView;
+    TextView mTextView;
+    CameraSource mCameraSource;
+    BarcodeDetector mBarcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        mBarcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        mCameraSource = new CameraSource.Builder(this, mBarcodeDetector)
+                .setRequestedPreviewSize(640, 480).build();
+
+        mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(CameraActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},122);
+                    return;
+                } else {
+
+                }
+//                try {
+//                    mCameraSource.start(holder);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                startCamera(holder);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mCameraSource.stop();
+            }
+
+        });
+
+        mBarcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> data = detections.getDetectedItems();
+                if(data.size()!=0){
+                    mTextView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextView.setText(data.valueAt(0).displayValue);
+                        }
+                    });
+                    System.out.println("DIsplay Value ====" + data.valueAt(0).displayValue);
+                }
+            }
+        });
+
+    }
+
+    public void startCamera(SurfaceHolder holder){
+        try {
+            mCameraSource.start(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 122:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+}
---
+++ app/src/main/java/com/wildlabs/tracklocation/MainActivity.java
@@ -0,0 +1,132 @@
+package com.wildlabs.tracklocation;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+//import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+    ImageView qrImage;
+    //    private FusedLocationProviderClient mFusedLocationClient;
+    String mLocation;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+    Button mTrackLocation, mGetQR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrackLocation = (Button) findViewById(R.id.trackLocation);
+        mGetQR = (Button) findViewById(R.id.generateQR);
+
+        System.out.println("Thos worked 1------------");
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        System.out.println("Thos worked 2------------");
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                System.out.println("Thos worked 3------------");
+                double lat = location.getLatitude();
+                double longitude = location.getLongitude();
+                mLocation = lat + ", " + longitude;
+                generateQRCode();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        mTrackLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            },121);
+            return;
+        } else {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, mLocationListener);
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, mLocationListener);
+        }
+
+    }
+
+    public void generateQRCode(){
+        System.out.println("Reached the generator");
+        qrImage = (ImageView) findViewById(R.id.qrImage);
+        QRGContents qrgContents = new QRGContents();
+        QRGEncoder qrgEncoder = new QRGEncoder(mLocation,null, QRGContents.Type.TEXT, 32);
+        try {
+            // Getting QR-Code as Bitmap
+            bitmap = qrgEncoder.encodeAsBitmap();
+            // Setting Bitmap to ImageView
+            qrImage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        try {
+            QRGSaver.save("", mLocation, bitmap, QRGContents.ImageType.IMAGE_JPEG);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 121:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mLocationManager.requestLocationUpdates("gps", 1000, 0, mLocationListener);
+                }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
